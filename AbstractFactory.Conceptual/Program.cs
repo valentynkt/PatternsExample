f492bedca9f96ab3676bc965fe8a8a2ba33b4ba6
Valentyn <valentinkit14@gmail.com>
2024-04-15 14:14:16 +0300

    add

--- AbstractFactory.Conceptual/Program.cs
+++ AbstractFactory.Conceptual/Program.cs
@@ -18,20 +18,22 @@ namespace AbstractFactory.Conceptual
         private static void RealExample()
         {
             IGUIFactory factory;
-            //this should be calculated from configs not hardcoded
+            // Configuration for OS type, should be set based on actual system settings
             string configOS = "Windows";
             var service = new SomeService(configOS);
-            //paint without knowing IGUIFactory. We can encapsulate this abstract factory in this moment and use method that will work with this factory
-            Console.WriteLine("The moment we can encapsulate all abstraction, factories and all elements but still render elements using this method");
+
+            // Paint GUI elements without direct access to the factory
+            Console.WriteLine("Rendering GUI elements using encapsulated factory methods.");
             service.PaintAllElements();
 
-            //in this moment we can call our higher abstractions and directly work with factory and abstraction elements but concrete factory and elements are still hidden from us
-            Console.WriteLine("The moment we know about abstractions and can work with them directly but concrete factories and products are hidden from us");
+            // Accessing factory and GUI elements through an abstraction layer
+            Console.WriteLine("Accessing GUI elements through factory abstraction.");
             factory = service.GetFactory();
             var button = factory.CreateButton();
             var checkBox = factory.CreateCheckBox();
             button.Paint();
             checkBox.Paint();
+
         }
 
         private static void ConceptualExample()
